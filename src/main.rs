0e796113c9946b465049e0fc6fb7b4db8e6ea773
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-04-18 19:25:18 -0400

    Working on SubMaps

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ use tile_set::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let tiles = TileMap::new(20, 20, |x, y| Tile{x, y, id: 0, top_id: None});
+    let tiles = TileMap::new(34, 34, |x, y| (uvec2(6, 0), None));
     
     let offset = Vec3{x: 100.0, y: 100.0, z: 0.0 };
+    
+    let texture = load_texture("C:\\dev\\Rust\\wave_function_collapse\\resources\\medieval_pixel_art_tileset\\TileSet.png").await.unwrap();
+    texture.set_filter(FilterMode::Nearest);
+    let tileset = TileSet::new(texture, 7, 44 + 8);
 
+    let mesh = tiles.to_mesh(offset, 10.0, &tileset);
 
     loop {
         clear_background(RED);
-        let mesh = tiles.to_mesh(offset, 10.0);
         draw_mesh(&mesh);
         next_frame().await
     }
--- src/tile_set.rs
+++ src/tile_set.rs
@@ -3,20 +3,22 @@ use std::ops::{Add, Mul};
 use macroquad::prelude::*;
 use macroquad::models::{Vertex, Mesh};
 
+const SUB_MAP_MAX_SIZE: UVec2 = UVec2{x: 25, y: 25};
+
 pub struct TileSet
 {
-    pub texture: Texture2D,
-    pub tile_size: f32,
-    pub width: u16,
-    pub height: u16
+    texture: Texture2D,
+    width: u16,
+    height: u16
 }
 
 pub struct Tile
 {
-    pub x: usize,
-    pub y: usize,
-    pub id: u16,
-    pub top_id: Option<u16>,
+    x: u16,
+    y: u16,
+    index: u16,
+    pub id: UVec2,
+    pub top_id: Option<UVec2>,
 }
 
 pub struct TileMap
@@ -26,21 +28,53 @@ pub struct TileMap
     tiles: Vec<Tile>
 }
 
-struct TileInfo
+struct SubMap
 {
-    index: u16,
-    x: u16,
-    y: u16,
-    id: u16,
-    top_id: Option<u16>,
+    width: u16,
+    height: u16,
+    x: usize,
+    y: usize,
+    tiles: Vec<Tile>
 }
 
-impl TileInfo
+impl SubMap
 {
-    fn new(x: u16, y: u16, index: u16, id: u16) -> Self
+    fn new<F: Fn(usize, usize)->(UVec2, Option<UVec2>)>(width: u16, height: u16, x: usize, y: usize, gen: F)
     {
-        TileInfo { index, x, y, id, top_id: None }
+        let mut tiles = Vec::with_capacity((width * height) as usize);
+        for xi in 0..width
+        {
+            for yi in 0..height
+            {
+                let ids = gen(xi as usize + x, yi as usize + y);
+                tiles.push(Tile::new(x as u16, y as u16, index as u16, ids.0, ids.1));
+            }
+        }
     }
+}
+
+impl TileSet
+{
+    pub fn new(texture: Texture2D, width: u16, height: u16) -> Self
+    {
+        TileSet { texture, width, height}
+    }
+
+    pub fn width(&self) -> u16 {self.width}
+    pub fn height(&self) -> u16 {self.height}
+    pub fn texture(&self) -> Texture2D {self.texture}
+}
+
+impl Tile
+{
+    fn new(x: u16, y: u16, index: u16, id: UVec2, top_id: Option<UVec2>) -> Self
+    {
+        Tile { x, y, index, id, top_id }
+    }
+
+    pub fn x(&self) -> u16 {self.x}
+    pub fn y(&self) -> u16 {self.y}
+    pub fn index(&self) -> u16 {self.index}
 
     fn get_triangles(&self) -> [u16; 6]
     {
@@ -56,33 +90,34 @@ impl TileInfo
         ]
     }
 
-    fn get_verticies(&self, offset: Vec3, scale: f32) -> [Vertex; 4]
+    fn get_verticies(&self, offset: Vec3, scale: f32, tileset: &TileSet) -> [Vertex; 4]
     {
         let gen = |xoffset, yoffset| 
         {
+            let tile_size = Vec2{x: tileset.texture.width() / tileset.width as f32, y: tileset.texture.height() / tileset.height as f32};
+            let uv_pos = Vec2{x: tile_size.x * (self.id.x as f32 + xoffset), y: tile_size.y * (self.id.y as f32 + yoffset)};
+            let uv = Vec2{x: uv_pos.x / tileset.texture.width(), y: uv_pos.y / tileset.texture.height()};
+
             let pos = Vec3{x: self.x as f32 + xoffset, y: self.y as f32 + yoffset, z: 0.0} * scale + offset;
-            Vertex{position: pos, uv: Vec2::ZERO, color: GRAY}
+            Vertex{position: pos, uv, color: WHITE}
         };
 
         [gen(0.0, 0.0), gen(1.0, 0.0), gen(0.0, 1.0), gen(1.0, 1.0)]
     }
 }
 
-fn get_index<T: Add<Output = T> + Mul<Output = T>>(x: T, y: T, width: T) -> T
-{
-    y * width + x
-}
-
 impl TileMap
 {
-    pub fn new<F: Fn(usize, usize)->Tile>(width: usize, height: usize, generator: F) -> Self
+    pub fn new<F: Fn(usize, usize)->(UVec2, Option<UVec2>)>(width: usize, height: usize, generator: F) -> Self
     {
         let mut tiles = Vec::with_capacity(width * height);
         for x in 0..width
         {
             for y in 0..height
             {
-                tiles.push(generator(x, y));
+                let index = y * width + x;
+                let ids = generator(x, y);
+                tiles.push(Tile::new(x as u16, y as u16, index as u16, ids.0, ids.1));
             }
         }
 
@@ -102,23 +137,20 @@ impl TileMap
         return &mut self.tiles[y * self.width + x]
     }
     
-    pub fn to_mesh(&self, offset: Vec3, scale: f32) -> Mesh
+    pub fn to_mesh(&self, offset: Vec3, scale: f32, tileset: &TileSet) -> Mesh
     {
         let mut verticies = Vec::with_capacity(self.height * self.width * 4);
         let mut triangles = Vec::with_capacity(self.width * self.height * 6);
-        let mut index = 0;
 
-        for x in 0..self.width - 1
+        for x in 0..self.width
         {
-            for y in 0..self.height - 1
+            for y in 0..self.height
             {
-                let info = TileInfo::new(x as u16, y as u16, index, self.at(x, y).id);
-                verticies.extend_from_slice(&info.get_verticies(offset, scale));
-                triangles.extend_from_slice(&info.get_triangles());
-                index += 1;
+                verticies.extend_from_slice(&self.at(x, y).get_verticies(offset, scale, tileset));
+                triangles.extend_from_slice(&self.at(x, y).get_triangles());
             }
         }
 
-        Mesh { vertices: verticies, indices: triangles, texture: None }
+        Mesh { vertices: verticies, indices: triangles, texture: Some(tileset.texture) }
     }
 }
